ddc46d72db1668e3062c4dbcc7bf302dd016d330
Salsabil44 <achisalsabil556@gmail.com>
2023-12-08 02:25:51 +0000

    c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -6,29 +6,34 @@
  *
  * @head: head of the list
  * @n: value of the element
- * Return: the address of the new element, or NULL on failure
+ * Return: the address of the new element
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-    dlistint_t *new = malloc(sizeof(dlistint_t));
+	dlistint_t *new;
+	dlistint_t *h;
 
-    if (!head || !new)
-        return (new ? (free(new), NULL) : NULL);
+	new = malloc(sizeof(dlistint_t));
 
-    new->n = n;
-    new->prev = NULL;
+	if (new == NULL)
+		return (NULL);
 
-    if (!*head)
-    {
-        *head = new;
-        new->next = NULL;
-    }
-    else
-    {
-        new->next = *head;
-        (*head)->prev = new;
-        *head = new;
-    }
+	new->n = n;
+	new->prev = NULL;
 
-    return (new);
+	h = *head;
+
+	if (h != NULL)
+	{
+		while (h->prev != NULL)
+			h = h->prev;
+	}
+
+	new->next = h;
+
+	if (h != NULL)
+		h->prev = new;
+
+	*head = new;
+	return (new);
 }
