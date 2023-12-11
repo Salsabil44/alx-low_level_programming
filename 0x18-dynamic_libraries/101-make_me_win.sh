af31a0537f1f71edb3dd9c56509fc50ad8f4f239
Salsabil44 <138724765+Salsabil44@users.noreply.github.com>
2023-12-11 10:10:42 +0100

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P https://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@github.com/Salsabil44/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
+wget -P /tmp https://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@github.com/Salsabil44/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
 export LD_PRELOAD=/tmp/libgiga.so
