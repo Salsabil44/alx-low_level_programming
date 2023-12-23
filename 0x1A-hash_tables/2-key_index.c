365f759c573e4bd7620b2efd4e17c9d997fb7894
Salsabil44 <achisalsabil556@gmail.com>
2023-12-22 17:09:02 -0800

    Hash

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,16 +1,16 @@
 #include "hash_tables.h"
 
 /**
- * key_inde - function that generates a distributed index
+ * key_index - function that generates a distributed index
  * according to a given hash
  *
  * @key: key passed
  * @size: size of the hash tables
  * Return: key index
  */
-unsigned long int key_inde(const unsigned char *key, unsigned long int size)
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	if (key == NULL || size == 0)
+	if (key == 0 || size == 0)
 		return (0);
 	return (hash_djb2(key) % size);
 }
